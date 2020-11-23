d231c20a915175b49f72bab573f4214d3682a3b5
Daniel Ugrin <ugrindaniel@gmail.com>
2020-11-23 18:26:24 +0100

    converted to component class

--- frontend/src/Album.js
+++ frontend/src/Album.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,13 +7,12 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Footer from './Footer';
 import Navbar from './Navbar';
-import React, { useState, useEffect } from 'react'
+import { withStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = theme => ({
   icon: {
     marginRight: theme.spacing(2),
   },
@@ -39,97 +38,91 @@ const useStyles = makeStyles((theme) => ({
   cardContent: {
     flexGrow: 1,
   },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
+});
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //array amiben object van
 //[{title: title, animations: animations},{title: title, animations: animations}]
 
+ const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjIiLCJyb2xlIjoiQ3VzdG9tZXIiLCJuYmYiOjE2MDYxNTAxOTcsImV4cCI6MTYwNjc1NDk5NywiaWF0IjoxNjA2MTUwMTk3fQ.tEnr30UCEguUO2ALOAiHHRHr0R7TFfIlxCYqvdOCNoI"
+ const requestOptions = {
+   method: 'GET',
+   headers: {
+     'Content-Type': 'application/json',
+     Authorization: 'Bearer ' + token,
+   },
+ };
 
-
-export default function Album() {
-  const classes = useStyles();
-  
-
-  const [state, setState] = useState({
-    animations : null
-    
-  });
-
-  
-    //event.preventDefault();
-    const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjIiLCJyb2xlIjoiQ3VzdG9tZXIiLCJuYmYiOjE2MDYwNjUxNDksImV4cCI6MTYwNjY2OTk0OSwiaWF0IjoxNjA2MDY1MTQ5fQ.pgTyKQEBCUZkd5xBTTAyjD6gJuwgz2nmC-_dk148bIA"
-    const requestOptions = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-      },
+class Album extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      animations: []
     };
+  }
 
+  componentDidMount(){
     fetch("https://localhost:8080/api/animations/", requestOptions)
     .then(response => {
-        response.json().then(data =>{setState({animations:data}); }) 
+         response.json().then(data =>{this.setState({animations:data}); console.log(this.state.animations)}) 
     });
+  }
 
-    console.log(state.animations[0])
-
-  
-
-  
-  return (
-    <React.Fragment >
-      <CssBaseline />
-      <Navbar />
-      
-      <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Animations
-            </Typography>
-          </Container>
-        </div>
+  render(){
+    const { classes } = this.props;
+    return (
+      <React.Fragment >
+        <CssBaseline />
+        <Navbar />
         
-        <Container className={classes.cardGrid} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4} lg={3}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={state.animations[0]}
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    {/* <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography> */}
-                  </CardContent>
-                  <CardActions>
-                      <Button size="small" color="primary" href={"/album/" + card}>
-                        View
+        <main>
+          {/* Hero unit */}
+          <div className={classes.heroContent}>
+            <Container maxWidth="sm">
+              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                Animations
+              </Typography>
+            </Container>
+          </div>
+          
+          <Container className={classes.cardGrid} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4} lg={3}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image={this.state.animations[0]}
+                      title="Image title"
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      {/* <Typography>
+                        This is a media card. You can use this section to describe the content.
+                      </Typography> */}
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" color="primary" href={"/album/" + card}>
+                          View
+                        </Button>
+                      <Button size="small" color="primary" href="#">
+                        Download
                       </Button>
-                    <Button size="small" color="primary" href="#">
-                      Download
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      <Footer />
-    </React.Fragment>
-  );
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
+
+export default withStyles(useStyles)(Album);
